37116893a67ee342745f704d9f8ea17dca2e939b
lexx54 <chapelexx54@gmail.com>
2021-01-19 15:23:50 -0400

    added new JS files

--- scripts/contact.js
+++ scripts/contact.js
@@ -78,4 +78,4 @@ export function contactMaker(){
   }
 }
 
-contactMaker();
+// contactMaker();
---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+import { contactMaker } from "./contact.js";
+
+const d=document;
+
+d.addEventListener("DOMContentLoaded",e=>{
+  contactMaker();
+})
